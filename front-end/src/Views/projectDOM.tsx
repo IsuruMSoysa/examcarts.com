6035bac61730709c182cb0eed525d94c0a77e98e
Isuru Soysa <isurumsoysa@gmail.com>
2021-05-21 13:15:43 +0530

    Initial Project created

---
+++ front-end/src/Views/projectDOM.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Container} from "react-bootstrap";
+
+
+const Library: React.FC = () => {
+    return (
+        <Container fluid>
+           <h1>Hello World</h1>
+        </Container>
+    );
+}
+
+export default Library;
