a7cef26c2fedad16c423b356bff19a971e48a9d4
Isuru Soysa <isurumsoysa@gmail.com>
2021-07-25 10:07:46 +0530

    Student Upcoming Branch Complete

---
+++ back-end/controllers/studentDashboard/upcomingstuexmas.js
@@ -0,0 +1,36 @@
+let upcomingexams = require('../../model/Exam/scheduleExam')
+let stuclass = require('../../model/Classes/Class_Student')
+
+
+exports.upcomingstuexmas = async (req,res) => {
+    const stuclasses = await stuclass
+        .find({studentId:req.body.studentIdNum});
+
+    let classIdArr = []
+    stuclasses.forEach( element => {
+        classIdArr.push(element.classId)});
+
+    let upcomingExams = []
+    for (const element of classIdArr) {
+         const object = await upcomingexams
+            .find({classObjId: element}).populate('classObjId')
+        upcomingExams.push(object)
+    }
+
+    let studentExams = []
+    upcomingExams.forEach(classObj => {
+        classObj.forEach(examObj =>{
+            studentExams.push(examObj);
+        })
+    })
+
+    if (stuclasses) {
+        res.status(200).send(
+             {message: "Exams found!" , status: true, upcomingexams : studentExams }
+        )
+    }else{
+        res.status(200).send(
+            {message: "No Requests found!" },
+        )
+    }
+}
--- front-end/src/Routes/Pages/Dashboard UI/StudentDashBoard/SideMenuSD.tsx
+++ front-end/src/Routes/Pages/Dashboard UI/StudentDashBoard/SideMenuSD.tsx
@@ -3,8 +3,6 @@ import {Button, ButtonGroup, InputGroup, Navbar} from "react-bootstrap";
 import "../../../../Views/projectDOM.scss"
 import "../Styles/DashBord.scss"
 import {Route, BrowserRouter as Router,Link} from "react-router-dom";
-import MyClassesDOM from "../Components/MyClassDOM"
-import {inspect} from "util";
 import {Dropdown, Icon, Sidenav,Nav} from 'rsuite';
 import "rsuite/dist/styles/rsuite-default.css";
 
@@ -13,55 +11,19 @@ const SideMenuSD: React.FC = () => {
 
 
     return (
-        // <Nav className="sideMenuItems  flex-column">
-        //     <ButtonGroup vertical>
-        //         <Button className="navLinkItem p-4">
-        //             <Link className="menuLink" to="/dashboard/student">My Classes</Link>
-        //         </Button>
-        //         <Button className="navLinkItem p-4">
-        //             <Link className="menuLink" to="/dashboard/student/addClass"  >Add Class</Link>
-        //         </Button>
-        //         <Button className="navLinkItem p-4">
-        //             <Link to="/exams" className="menuLink" >My Papers</Link>
-        //         </Button>
-        //         <Button className="navLinkItem p-4">
-        //             <Link to="/exams" className="menuLink" >Upcoming Exams</Link>
-        //         </Button>
-        //         <Button className="navLinkItem p-4">
-        //             <Link to="/exams" className="menuLink"  >Results Release</Link>
-        //         </Button>
-        //         <Button className="navLinkItem p-4">
-        //             <Link to="/exams" className="menuLink" >Notifications</Link>
-        //         </Button>
-        //         <Button className="navLinkItem p-4">
-        //             <Link to="/exams"  className="menuLink" >Settings</Link>
-        //         </Button>
-        //     </ButtonGroup>
-        // </Nav>
-
-        <div>
-            <Sidenav  style={{backgroundColor:"#3F6844" }} defaultOpenKeys={['3', '4']}>
-                <Sidenav.Body>
-                    <Nav>
-                        <Link  to="/dashboard/student" className="navMenuItem" >
-                        <Nav.Item  eventKey="1" >
-                                My Classes
-                        </Nav.Item>
-                        </Link>
-                        <Link className="navMenuItem" to="/dashboard/student/addClass"  >
-                        <Nav.Item  eventKey="2" >
-                           Add Class
-                        </Nav.Item>
-                        </Link>
-                        <Link className="navMenuItem" to="/exams" >
-                        <Nav.Item  eventKey="2" >
-                            My Papers
-                        </Nav.Item>
-                        </Link>
-                    </Nav>
-                </Sidenav.Body>
-            </Sidenav>
-        </div>
+      <Nav className="sideMenuItems pl-3 flex-column">
+          <ButtonGroup vertical>
+              <Button className="navLinkItem p-4">
+                  <Link className="menuLink" to="/dashboard/student"> My Classes</Link>
+              </Button>
+              <Button className="navLinkItem p-4">
+                  <Link className="menuLink" to="/dashboard/student/addClass"> Add Class</Link>
+              </Button>
+              <Button className="navLinkItem p-4">
+                  <Link className="menuLink" to="/dashboard/student/upcomingexam"> Upcoming Exams</Link>
+              </Button>
+          </ButtonGroup>
+      </Nav>
     );
 }
 
